6db03af296c4b3e86019b9b115dee2d13e1d8ced
moodelymoo <moodelymoo@gmail.com>
2020-12-16 13:19:36 +0000

    Some cleanup of code, removed some non project files

--- Code/YourImpSpace/FizzBuzz.py
+++ Code/YourImpSpace/FizzBuzz.py
@@ -2,7 +2,6 @@ from Code.Examples.FizzBuzz import fizzBuzzOneLine
 
 
 def main():
-    fizzBuzzOneLine()
     # place your code here
     # print all output to the console
     # run Autochecker/FizzBuzzChecker to test if your solution passes
